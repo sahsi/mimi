bbc78da101f48934d6b3176157a7cfe5893130b7
sahsi <partacav@gmail.com>
2016-08-30 08:14:33 -1200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,105 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>ALL-ANIMEY - Conditions générales</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <!-- bootstrap -->
+<link href="assets/css/accueil/bootstrap.min.css" rel='stylesheet' type='text/css' media="all" />
+<!-- //bootstrap -->
+<link href="assets/css/dashboard.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<link href="assets/css/style.css" rel='stylesheet' type='text/css' media="all" />
+<script src="assets/js/jquery-1.11.1.min.js"></script>
+<!--start-smoth-scrolling-->
+<!-- fonts -->
+<link href='assets/css/fonts1.css' rel='stylesheet' type='text/css'>
+<link href='assets/css/fonts2.css' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css" type="text/css">
+<link rel="stylesheet" href="assets/css/bootstrap.203.css"> 
+<!-- //fonts -->
+<!-- Plugin CSS -->
+<link rel="stylesheet" href="assets/css/animate.min.css" type="text/css">
+<!-- Custom CSS -->
+<link rel="stylesheet" href="assets/css/creative.css" type="text/css">
+<!-- icons -->
+<link rel="shortcut icon" href="favicon.ico" >
+<link rel="icon" type="image/png" href="img/favicon-32x32.png" >
+ 
+<style>
+	
+	
+.about {
+  
+	margin: 0;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 13px;
+    line-height: 18px;
+    color: #333333;
+    background-color: #ffffff;
+}
+</style>
+</head>
+  <body>
+<?php  $section="accueil";
+     include("header_2.php");  
+		  include("nav_bar_vis.php");
+		  
+		  ?>
+      
+	  <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">	 
+	 
+        <div class="container">
+            <div class="row">	
+			
+			<div class="col-lg-12  ">
+			
+			<H1 style="color : #FF6600; font-family: Palatino;"> Conditions générales</h1>
+			<br>
+			<br>
+			
+			<div class="about">
+			<h4> Afin d'accéder à certaines fonctionnalités et services, ainsi que pour envoyer contenus et messages sur le site, vous devez créer un compte ALL-ANIMEY et devenir membre. </h4>
+			<h4>Vous vous engagez à ne pas utiliser ce Site ou ces Services pour proposer, exposer, distribuer, transmettre, envoyer, ou fournir des connexions ou pour enregistrer aucun matériel qui enfreint les œuvres et leurs copyright ou encore qui transgresse ou fait la promotion de la violation des droits à la propriété intellectuelle d'un tiers intervenant. Crunchyroll a adopté et instauré une politique qui résilie dans des conditions appropriées les comptes des utilisateurs qui enfreignent à maintes reprises ou qui sont suspectés ou accusés à maintes reprises d'enfreindre les copyright et les ayants droit. </h4>
+			<br>
+			</div>
+			<div class="container">
+            <div class="row" >
+			
+		   
+			
+			
+			
+			
+			</div>
+			</div>
+			</div>
+	
+			
+			
+		</div>
+     
+		<br><br><br><br>
+
+	
+	   </div>
+
+
+	<script type="text/javascript" src="../assets/js/jqBootstrapValidation.js"></script>
+	<script>
+  $(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(); } );
+</script>
+	
+<?php include("footer.php");?>
+			
+</div>
+   <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+
+	
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+  </body>
+</html>
